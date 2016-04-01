7c9647ceec49c99a8f296f5be0882a307fa91577
maunilmehta <maunilmehta@Maunil>
2016-04-01 16:55:32 -0700

    Controller changes

--- src/main/java/com/journaldev/spring/ChallengeController.java
+++ src/main/java/com/journaldev/spring/ChallengeController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ChallengeController {
 
-	@RequestMapping(value = "/challenges", method = RequestMethod.GET)
-	public String challenges() {
-
-		return "challenges";
-
-	}
 	
 	@RequestMapping(value = "/createChallenge", method = RequestMethod.GET)
 	public String createChallenge() {
